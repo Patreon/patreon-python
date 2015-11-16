1155e6f6d50ec698081c6f8d745bc1f8f8c13ccc
David Kettler <21echoes@gmail.com>
2015-11-16 10:46:29 -0800

    add a way to fetch campaign only, no patrons

--- patreon/api.py
+++ patreon/api.py
@@ -11,6 +11,9 @@ class API(object):
   def fetch_campaign_and_patrons(self):
     return self.__get_json('current_user/campaigns?include=rewards,creator,goals,pledges')
 
+  def fetch_campaign(self):
+    return self.__get_json('current_user/campaigns?include=rewards,creator,goals')
+
   def __get_json(self, suffix):
     response = requests.get("https://api.patreon.com/oauth2/api/{}".format(suffix),
       headers={'Authorization': "Bearer {}".format(self.access_token)})
