2efccbc09a5b141d2a371a9f03074d1547adff4a
Daniel Samuels <daniel.samuels1@gmail.com>
2016-03-19 22:10:55 +0000

    Parse the cursor for the next page and return it if requested.

--- patreon/api.py
+++ patreon/api.py
@@ -1,5 +1,6 @@
 import urllib
 from datetime import timezone
+from urllib.parse import parse_qs, urlparse
 
 import requests
 
@@ -19,19 +20,32 @@ class API(object):
     def fetch_campaign(self):
         return self.__get_json('current_user/campaigns?include=rewards,creator,goals')
 
-    def fetch_page_of_pledges(self, campaign_id, page_size, cursor=None):
+    def fetch_page_of_pledges(self, campaign_id, page_size, cursor=None, return_cursor=False):
         url = 'campaigns/{0}/pledges?page%5Bcount%5D={1}'.format(campaign_id, str(page_size))
+
         if cursor:
             try:
                 cursor = self.__as_utc(cursor).isoformat()
             except AttributeError:
                 pass
             url += '&page%5Bcursor%5D={0}'.format(urllib.parse.quote(cursor))
+
+        if return_cursor:
+            data = self.__get_json(url)
+
+            # Extract the 'cursor'
+            if 'next' in data['links']:
+                query_string = urlparse(data['links']['next']).query
+                parsed_query_string = parse_qs(query_string)
+                cursor = parsed_query_string['page[cursor]'][0]
+
+            return data, cursor
         return self.__get_json(url)
 
     def __get_json(self, suffix):
-        response = requests.get("https://api.patreon.com/oauth2/api/{}".format(suffix),
-                                headers={'Authorization': "Bearer {}".format(self.access_token)})
+        response = requests.get("https://api.patreon.com/oauth2/api/{}".format(suffix), headers={
+            'Authorization': "Bearer {}".format(self.access_token)
+        })
         return response.json()
 
     @staticmethod
