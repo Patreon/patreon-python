f41e264756d22d95787aba3156e13939b7721652
David Kettler <21echoes@gmail.com>
2016-06-19 13:40:21 -0700

    create schemas for the available resources so that users can see what resources and fields are available, and request them as needed.

--- patreon/api.py
+++ patreon/api.py
@@ -1,6 +1,7 @@
 import requests
 
 from patreon.jsonapi.url_util import build_url
+from patreon.schemas import campaign
 from patreon.version_compatibility.utc_timezone import utc_timezone
 from patreon.version_compatibility.urlencode import urlencode
 
@@ -10,26 +11,18 @@ class API(object):
     super(API, self).__init__()
     self.access_token = access_token
 
-  def fetch_user(self):
-    return self.__get_json(build_url('current_user'))
+  def fetch_user(self, includes=None, fields=None):
+    return self.__get_json(build_url('current_user', includes=includes, fields=fields))
 
-  def fetch_campaign_and_patrons(self):
-    return self.__get_json(
-      build_url(
-        'current_user/campaigns',
-        includes=['rewards', 'creator', 'goals', 'pledges']
-      )
-    )
+  def fetch_campaign_and_patrons(self, includes=None, fields=None):
+    if not includes:
+      includes = campaign.default_relationships + [campaign.Relationships.pledges]
+    return self.fetch_campaign(includes=includes, fields=fields)
 
-  def fetch_campaign(self):
-    return self.__get_json(
-      build_url(
-        'current_user/campaigns',
-        includes=['rewards', 'creator', 'goals']
-      )
-    )
+  def fetch_campaign(self, includes=None, fields=None):
+    return self.__get_json(build_url('current_user/campaigns', includes=includes, fields=fields))
 
-  def fetch_page_of_pledges(self, campaign_id, page_size, cursor=None):
+  def fetch_page_of_pledges(self, campaign_id, page_size, cursor=None, includes=None, fields=None):
     url = 'campaigns/{0}/pledges'.format(campaign_id)
     params = {'page[count]': page_size}
     if cursor:
@@ -39,7 +32,7 @@ class API(object):
         pass
       params.update({'page[cursor]': cursor})
     url += "?" + urlencode(params)
-    return self.__get_json(build_url(url))
+    return self.__get_json(build_url(url, includes=includes, fields=fields))
 
   def __get_json(self, suffix):
     response = requests.get(
