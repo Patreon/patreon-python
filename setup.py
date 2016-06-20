3c24c9fe54fae6c4895446594303e1f94e0b0ab3
David Kettler <21echoes@gmail.com>
2016-06-20 14:43:45 -0700

    version bump to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='patreon',
-      version='0.1.0',
+      version='0.2.0',
       description='Python library for interacting with the Patreon API. OAuth-centric for now.',
       url='http://github.com/Patreon/patreon-python',
       author='Patreon',
