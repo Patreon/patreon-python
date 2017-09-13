a9d15bc99d2a14c28a5171feae2ce2a39f1386ca
Bailey Stoner <bailey@patreon.com>
2017-09-12 21:22:00 -0700

    Clean up schema specs.

--- patreon/schemas/goal_spec.py
+++ patreon/schemas/goal_spec.py
@@ -22,25 +22,13 @@ def relationships():
 def test_schema_attributes_are_properly_formatted(attributes):
     for attribute_name in attributes:
         value = getattr(goal.Attributes, attribute_name, None)
-
-        if value is None:
-            raise ValueError(
-                '{} should be in attributes'.format(attribute_name),
-            )
-
-        assert value is attribute_name
+        assert value is not None and value is attribute_name
 
 
 def test_schema_relationships_are_properly_formatted(relationships):
     for relationship_name in relationships:
         value = getattr(goal.Relationships, relationship_name, None)
-
-        if value is None:
-            raise ValueError(
-                '{} should be in relationships'.format(relationship_name),
-            )
-
-        assert value is relationship_name
+        assert value is not None and value is relationship_name
 
 
 def test_schema_has_expected_default_attributes():
