523778442ebb55bab0cdd31654f79f2184a0214f
David Kettler <21echoes@gmail.com>
2017-10-23 20:35:18 -0700

    version bump now that we have a different return type

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DESCRIPTION = (
 
 setup(
     name='patreon',
-    version='0.3.1',
+    version='0.4.0',
     description=DESCRIPTION,
     url='http://github.com/Patreon/patreon-python',
     author='Patreon',
